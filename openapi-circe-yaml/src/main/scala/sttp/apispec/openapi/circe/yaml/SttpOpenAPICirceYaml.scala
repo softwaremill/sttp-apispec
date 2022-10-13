defc2031c73ce790db1b5a147aa41cc6a730cb24
Rafal Ambrozewicz <rafal.ambrozewicz@gmail.com>
2022-10-13 14:53:58 +0200

    Introduced `RichOpenAPI3_1` implicit class offering `toYaml` extension method for generation OpenAPI 3.1.0 yaml string.

--- openapi-circe-yaml/src/main/scala/sttp/apispec/openapi/circe/yaml/SttpOpenAPICirceYaml.scala
+++ openapi-circe-yaml/src/main/scala/sttp/apispec/openapi/circe/yaml/SttpOpenAPICirceYaml.scala
@@ -4,10 +4,20 @@ import io.circe.syntax._
 import io.circe.yaml.Printer
 import io.circe.yaml.Printer.StringStyle
 import sttp.apispec.openapi.OpenAPI
-import sttp.apispec.openapi.circe._
 
 trait SttpOpenAPICirceYaml {
+
   implicit class RichOpenAPI(openAPI: OpenAPI) {
+    import sttp.apispec.openapi.circe._
+
+    def toYaml: String = Printer(dropNullKeys = true, preserveOrder = true).pretty(openAPI.asJson)
+    def toYaml(stringStyle: StringStyle): String =
+      Printer(dropNullKeys = true, preserveOrder = true, stringStyle = stringStyle).pretty(openAPI.asJson)
+  }
+
+  implicit class RichOpenAPI3_1(openAPI: OpenAPI) {
+    import sttp.apispec.openapi.circe_openapi_3_1._
+
     def toYaml: String = Printer(dropNullKeys = true, preserveOrder = true).pretty(openAPI.asJson)
     def toYaml(stringStyle: StringStyle): String =
       Printer(dropNullKeys = true, preserveOrder = true, stringStyle = stringStyle).pretty(openAPI.asJson)
