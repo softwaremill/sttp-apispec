744c263341fa8568bbba7c3c07b1a57b9224e487
Rafal Ambrozewicz <rafal.ambrozewicz@gmail.com>
2022-10-13 14:51:22 +0200

    Next to the default `circe` package object extending OpenAPI 3.0.3 encoder/decoders added `circe_openapi_3_1` that is extedng OpenAPI 3.1.0 encoder/decoders.

--- openapi-circe/src/main/scala/sttp/apispec/openapi/circe/package.scala
+++ openapi-circe/src/main/scala/sttp/apispec/openapi/circe/package.scala
@@ -1,8 +1,13 @@
 package sttp.apispec.openapi
 
 import sttp.apispec.AnySchema
+import sttp.apispec.openapi.circe.{SttpOpenAPI3_1CirceEncoders, SttpOpenAPICirceDecoders}
 
 package object circe extends SttpOpenAPICirceEncoders with SttpOpenAPICirceDecoders {
+  override val anyObjectEncoding: AnySchema.Encoding = AnySchema.Encoding.Boolean
+}
+
+package object circe_openapi_3_1 extends SttpOpenAPI3_1CirceEncoders with SttpOpenAPICirceDecoders {
   override val anyObjectEncoding: AnySchema.Encoding = AnySchema.Encoding.Boolean
 }
 
